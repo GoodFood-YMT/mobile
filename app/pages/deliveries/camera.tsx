3212ca642e41dfbf160c8f4202fb9ae6158c7b8f
tom-brulin <t.brulin@outlook.com>
2024-04-10 10:42:26 +0200

    feat(delivery): take photo with camera

--- app/pages/deliveries/camera.tsx
+++ app/pages/deliveries/camera.tsx
@@ -5,13 +5,17 @@ import { Button } from "../../components/ui/button";
 import { useCompleteDelivery } from "../../hooks/delivery/use_complete_delivery";
 import Toast from "react-native-toast-message";
 import Icon from "react-native-vector-icons/SimpleLineIcons";
+import { Camera, CameraType } from "expo-camera";
+import { useEffect } from "react";
 
 const StyledView = styled(View);
 const StyledImageBackground = styled(ImageBackground);
 const StyledPressable = styled(Pressable);
 const StyledText = styled(Text);
+const StyledCamera = styled(Camera);
 
 export default function OpenCameraView() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
   const { deliveryId } = useParams();
   const completeDelivery = useCompleteDelivery();
   const navigate = useNavigate();
@@ -44,14 +48,44 @@ export default function OpenCameraView() {
     navigate(`/deliveries/${deliveryId}/itinarery`);
   };
 
+  useEffect(() => {
+    async function requestCameraPermission() {
+      await requestPermission();
+    }
+
+    requestCameraPermission();
+  }, []);
+
+  if (!permission || !permission.granted) {
+    return (
+      <StyledImageBackground
+        source={{
+          uri: "https://images.unsplash.com/photo-1617500603321-bcd6286973b7?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        }}
+        resizeMode="cover"
+        className="h-screen w-screen relative"
+      >
+        <StyledView className="absolute w-screen pt-20 px-8 pb-6 bg-white">
+          <StyledPressable
+            onPress={handleBack}
+            className="flex flex-row items-center"
+          >
+            <Icon name="arrow-left" size={16} color="black" />
+            <StyledText className="text-black text-sm ml-2">Back</StyledText>
+          </StyledPressable>
+        </StyledView>
+        <StyledView className="absolute bottom-0 w-screen p-8">
+          <Button
+            title="Complete the delivery"
+            onPress={handleCompleteDelivery}
+          />
+        </StyledView>
+      </StyledImageBackground>
+    );
+  }
+
   return (
-    <StyledImageBackground
-      source={{
-        uri: "https://images.unsplash.com/photo-1617500603321-bcd6286973b7?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      }}
-      resizeMode="cover"
-      className="h-screen w-screen relative"
-    >
+    <StyledCamera className="h-screen w-screen relative" type={CameraType.back}>
       <StyledView className="absolute w-screen pt-20 px-8 pb-6 bg-white">
         <StyledPressable
           onPress={handleBack}
@@ -67,6 +101,6 @@ export default function OpenCameraView() {
           onPress={handleCompleteDelivery}
         />
       </StyledView>
-    </StyledImageBackground>
+    </StyledCamera>
   );
 }
